b5e69212c356a1511e95e672b0e8245643d364d6
seanthony <sean@basecampcodingacademy.org>
2018-11-16 10:18:21 -0600

    correctly searing for artist and is returning the related artists with genres, about to try and implement first iteration of bkeh plot

--- spotify_explorer.py
+++ spotify_explorer.py
@@ -1,7 +1,26 @@
-from secrets import client_id, secret
+from secrets import username, scope, client_id, secret, redirect
 import spotipy
+import spotipy.util as util
 
-spotify = spotipy.Spotify()
+token = util.prompt_for_user_token(
+    username, scope, client_id=client_id, client_secret=secret, redirect_uri=redirect)
+
+spotify = spotipy.Spotify(auth=token)
+
+
 name = 'Jason Molina'
-results = spotify.search(q='artist:' + name, type='artist')
-print(results)
+artist = spotify.search(q='artist:' + name, type='artist')
+
+# if artist.get('artists', False) and artist['artists'].get('items', False) and artist['artists']['items'][0].get('id', False):
+id = artist['artists']['items'][0].get('id')
+related = spotify.artist_related_artists(id)
+name = artist['artists']['items'][0].get('name')
+main_genres = artist['artists']['items'][0].get('genres')
+genres = set(main_genres)
+artists = [{'artist': name, 'genres': main_genres}]
+for artist_dict in related.get('artists', []):
+    artist_name = artist_dict.get('name')
+    artist_genres = artist_dict.get('genres', [])
+    for genre in artist_genres:
+        genres.add(genre)
+    artists.append({'artist': artist_name, 'genres': artist_genres})
