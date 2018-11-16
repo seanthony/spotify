1d3aa6ee23de979b63c857fc5ed64d06e79c5cec
seanthony <sean@basecampcodingacademy.org>
2018-11-16 10:37:56 -0600

    first iteration of the bokeh plot showing related categories of artists and genres

--- spotify_explorer.py
+++ spotify_explorer.py
@@ -2,6 +2,9 @@ from secrets import username, scope, client_id, secret, redirect
 import spotipy
 import spotipy.util as util
 
+import numpy as np
+from bokeh.plotting import figure, show, output_file
+
 token = util.prompt_for_user_token(
     username, scope, client_id=client_id, client_secret=secret, redirect_uri=redirect)
 
@@ -16,11 +19,63 @@ id = artist['artists']['items'][0].get('id')
 related = spotify.artist_related_artists(id)
 name = artist['artists']['items'][0].get('name')
 main_genres = artist['artists']['items'][0].get('genres')
-genres = set(main_genres)
+genre_set = set(main_genres)
 artists = [{'artist': name, 'genres': main_genres}]
-for artist_dict in related.get('artists', []):
+for artist_dict in related.get('artists', [])[:8]:
     artist_name = artist_dict.get('name')
     artist_genres = artist_dict.get('genres', [])
     for genre in artist_genres:
-        genres.add(genre)
+        genre_set.add(genre)
     artists.append({'artist': artist_name, 'genres': artist_genres})
+
+genres = sorted(list(genre_set))
+names = [artist['artist'] for artist in artists]
+
+colors = ['#ffffff', '#1db954']
+xname = []
+yname = []
+color = []
+alpha = []
+
+for genre in genres:
+    for artist in artists:
+        xname.append(genre)
+        yname.append(artist['artist'])
+        alpha.append(1)
+        if genre in artist.get('genres', []):
+            color.append(colors[1])
+        else:
+            color.append(colors[0])
+
+
+data = dict(
+    xname=xname,
+    yname=yname,
+    colors=color,
+    alphas=alpha,
+    # count=counts.flatten(),
+)
+
+p = figure(title="Related Artists on Spotify",
+           x_axis_location="above", tools="hover,save",
+           x_range=genres, y_range=list(reversed(names)),)
+#    tooltips=[('names', '@yname, @xname'), ('count', '@count')])
+
+# p.plot_width = 800
+# p.plot_height = 800
+p.sizing_mode = "stretch_both"
+p.grid.grid_line_color = None
+p.axis.axis_line_color = None
+p.axis.major_tick_line_color = None
+p.axis.major_label_text_font_size = "5pt"
+p.axis.major_label_standoff = 0
+p.xaxis.major_label_orientation = np.pi / 3
+
+p.rect('xname', 'yname', 0.9, 0.9, source=data,
+       color='colors', alpha='alphas', line_color=None,
+       hover_line_color='black', hover_color='colors')
+
+title = 'Related Artists for {}'.format(name)
+output_file("index.html", title=title)
+
+show(p)  # show the plot
